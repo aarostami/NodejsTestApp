76f920b885a87891371261f6624128245ddb5ad9
aarostami <71734012+aarostami@users.noreply.github.com>
2025-03-09 19:31:19 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 /* tslint:disable no-var-requires */
 const express = require('express')
 // const fs = require('fs')
